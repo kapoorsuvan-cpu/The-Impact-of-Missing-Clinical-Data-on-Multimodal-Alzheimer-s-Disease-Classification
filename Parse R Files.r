2d5f9d79c99b53b0538848daadc63b37f200d02d
suvank <kapoor_suvan@berkeley.edu>
2026-05-27 14:56:35 -0700

    Add files via upload

---
+++ Parse R Files.r
@@ -0,0 +1,31 @@
+setwd("/Users/suvankapoor/Documents/Multimodal Alzheimer's Research")
+
+# load the file
+obj_name <- load("ADNIMERGE2/data/ADSL.rda")
+
+print(obj_name)   # shows object name
+
+# extract object
+df <- get(obj_name)
+
+# write CSV
+write.csv(df, "ADSL.csv", row.names = FALSE)
+
+# confirm it exists
+print(list.files())
+
+setwd("/Users/suvankapoor/Documents/Multimodal Alzheimer's Research")
+
+# load the file
+obj_name <- load("ADNIMERGE2/data/UCSFFSX6.rda")
+
+print(obj_name)   # shows object name
+
+# extract object
+df <- get(obj_name)
+
+# write CSV
+write.csv(df, "UCSFFSX6.csv", row.names = FALSE)
+
+# confirm it exists
+print(list.files())
